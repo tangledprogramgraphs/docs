d7ba54dfd460ceba39661d8f036e0451f507a269
richardl2003 <richardliy03@gmail.com>
2025-04-12 16:31:56 -0400

    Fix broken links and adjust link handling

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   organizationName: "tangledprogramgraphs", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
